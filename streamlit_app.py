01eb2b981c5a245d7e22a6371d6c8d906a496223
hynsam <56837907+hynsam@users.noreply.github.com>
2024-10-16 23:02:52 -0400

    completed

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
+from sklearn.manifold import TSNE
 
 
 @st.cache
@@ -13,6 +14,132 @@ def load_data():
     return df
 
 df = load_data()
+
+df["Obesity"] = (df['Body Mass Index (BMI)'] >= 30).astype(int)
+df["Hypertension"] = ((df['Systolic Blood Pressure (mmHg)'] >= 130) | (df['Diastolic Blood Pressure (mmHg)'] >= 80) | 
+                          (df['Ever Told Had High Blood Pressure'] == 'Yes') | (df['Now Taking Hypertension Medication'] == 'Yes')).astype(int)
+df["Diabetes"] = ((df['Glycohemoglobin (A1C) %'] >= 6.5) | (df['Doctor Told You Have Diabetes'] == 'Yes')).astype(int)
+df['High Cholesterol'] = ((df['Total Cholesterol (mg/dL)'] >= 240) | (df['Calculated LDL Cholesterol (mg/dL)'] >= 160) | 
+                          (df['Triglycerides (mg/dL)'] >= 200) | (df['Now Taking Hypertension Medication'] == 'Yes')).astype(int)
+
+# Title of the Plot
+st.write("## NHANES")
+
+### Task 1 ###
+
+# Disease selector
+diseases_list = [
+    "Obesity",
+    "Diabetes",
+    "Hypertension",
+    "High Cholesterol"
+]
+disease_option  = st.selectbox("Select a Disease", diseases_list)
+
+# Age range slider
+age_min, age_max  = st.slider("Select Age Range of Participants", 
+                              min_value=int(df['Age of the Participant (years)'].min()), 
+                              max_value=int(df["Age of the Participant (years)"].max()), 
+                              value=(int(df['Age of the Participant (years)'].min()), int(df['Age of the Participant (years)'].max())))
+# Filter dataset based on the selected age range
+subset = df[(df['Age of the Participant (years)'] >= age_min) & (df['Age of the Participant (years)'] <= age_max)]
+
+# a donut chat for the gender groups
+gender_disease_counts = subset.groupby('Gender of the Participant')[[disease_option]].sum().reset_index()
+gender_donut = alt.Chart(gender_disease_counts).mark_arc(innerRadius=50, outerRadius=90).encode(
+    theta=alt.Theta(field=disease_option, type='quantitative', aggregate='sum'),
+    color=alt.Color('Gender of the Participant:N', legend=alt.Legend(title="Gender"), 
+                    scale=alt.Scale(domain=['Female', 'Male'], range=['#4C78A8', '#F28E2C'])),
+    tooltip=[alt.Tooltip('Gender of the Participant:N', title='Gender'),
+             alt.Tooltip(field=disease_option, title=f'{disease_option} Cases')]
+).properties(
+    title=f'Number of {disease_option} Cases by Gender',
+    width=250
+)
+# a donut chat for the race groups
+race_disease_counts = subset.groupby('Race/Ethnicity')[[disease_option]].sum().reset_index()
+race_donut = alt.Chart(race_disease_counts).mark_arc(innerRadius=50, outerRadius=90).encode(
+    theta=alt.Theta(field=disease_option, type='quantitative', aggregate='sum'),
+    color=alt.Color('Race/Ethnicity:N', legend=alt.Legend(title="Race")),
+    tooltip=[alt.Tooltip('Race/Ethnicity:N', title='Race'),
+             alt.Tooltip(field=disease_option, title=f'{disease_option} Cases')]
+).properties(
+    title=f'Number of {disease_option} Cases by Race',
+    width=250
+)
+
+donut = alt.hconcat(gender_donut, race_donut).resolve_scale(
+    # two donut charts should use different color schema
+    color='independent'
+)
+
+age_disease_counts = subset.groupby('Age of the Participant (years)')[[disease_option]].sum().reset_index()
+age_distribution_chart = alt.Chart(age_disease_counts).mark_bar().encode(
+    x=alt.X('Age of the Participant (years):O', title="Age of Participants", axis=alt.Axis(labelAngle=0)),
+    y=alt.Y(disease_option + ':Q', title=f'Number of {disease_option} Cases'),
+    tooltip=[alt.Tooltip('Age of the Participant (years):O', title='Age'),
+             alt.Tooltip(field=disease_option, title=f'{disease_option} Cases')]
+).properties(
+    title=f'Distribution of {disease_option} Cases by Age',
+    width=600
+)
+
+# Calculate the prevalence as percentages within each group
+grouped_df = subset.groupby(["Gender of the Participant", "Race/Ethnicity"]).agg(
+    total = ('Respondent', 'size'),
+    disease_total = (disease_option, 'sum')
+).reset_index()
+grouped_df['prevalence'] = (grouped_df['disease_total'] / grouped_df['total']) * 100
+
+all_genders = subset.groupby("Race/Ethnicity").agg(
+    total=('Respondent', 'size'),
+    disease_total=(disease_option, 'sum')
+).reset_index()
+all_genders['Gender of the Participant'] = 'All'
+all_genders['prevalence'] = (all_genders['disease_total'] / all_genders['total']) * 100
+final_df = pd.concat([grouped_df, all_genders])
+
+# Create a selection component
+race_selection = alt.selection_multi(fields=['Race/Ethnicity'], bind='legend')
+
+# Create the prevalence chart with bars
+prevalence_chart = alt.Chart(final_df).mark_bar().encode(
+    x=alt.X('Race/Ethnicity:N', title=None, axis=alt.Axis(labels=False)),
+    y=alt.Y('prevalence:Q', title='Prevalence (%)'),
+    color=alt.condition(race_selection, 'Race/Ethnicity:N', alt.value('lightgray')),
+    tooltip=[alt.Tooltip('Race/Ethnicity:N', title='Race/Ethnicity'),
+             alt.Tooltip('prevalence:Q', title='Prevalence (%)', format='.2f')]
+).add_selection(
+    race_selection
+).properties(width = 200)
+
+# Add text labels showing the prevalence on top of each bar
+text = prevalence_chart.mark_text(
+    align='center',
+    baseline='bottom',
+    dy=-3  # Adjust the vertical position of the text
+).encode(
+    text=alt.Text('prevalence:Q', format='.2f')  # Format to 2 decimal places
+)
+
+# Layer the bars and text labels together
+layered_prevalence_chart = alt.layer(prevalence_chart, text)
+
+# Now facet the layered chart by gender
+faceted_chart = layered_prevalence_chart.facet(
+    column=alt.Column('Gender of the Participant:N', header=alt.Header(title="Gender", labelOrient='bottom'))
+).resolve_scale(
+    x='independent'
+).properties(
+    title=f'Prevalence of {disease_option} by Sex and Race'
+)
+
+# Display the chart in Streamlit
+st.altair_chart(donut)
+st.altair_chart(age_distribution_chart)
+st.altair_chart(faceted_chart)
+
+### Task 1 ###
 
 # a version of df where all varaibles are number, if categorical, they are encoded as numbers
 df_num = df.copy()
@@ -78,12 +205,12 @@ st.write("## Principal Component Analysis")
 lifestyle_factors_of_interest = st.multiselect(
     "Select Lifestyle Factors",
     options=lifestyle_factors,
-    default=['Sleep Duration (hours)', 'Frequency of Muscle-Strengthening Activities per Week', 'Ever Had at Least 12 Alcoholic Drinks in One Year', 'Current Smoking Frequency', 'Time Spent Watching TV or Videos (minutes/day)', 'Calcium Intake (mg)', 'Alcohol Intake (grams)', 'Sugar Intake (grams)', 'Total Fat Intake (grams)', 'Engaged in Vigorous Activity in Past 30 Days', 'Frequency of Vigorous Physical Activity per Week', 'Smoked at Least 100 Cigarettes in Life']
+    default=['Sugar Intake (grams)', 'Total Fat Intake (grams)', 'Alcohol Intake (grams)', 'Current Smoking Frequency', 'Smoked at Least 100 Cigarettes in Life']
 )
 health_outcome_of_interest = st.selectbox(
     "Select Health Outcome",
     options=health_outcomes,
-    index=health_outcomes.index('Ever Told Had High Blood Pressure')
+    index=health_outcomes.index('Weight (kg)')
 )
 
 # pca on all the lifestyle factors
@@ -97,6 +224,9 @@ X_pca = pca.fit_transform(X_scaled)
 df_pca = pd.DataFrame(X_pca, columns=['PC1', 'PC2'])
 df_pca['Health Outcome'] = df[health_outcome_of_interest]
 
+# drop all nan
+df_pca = df_pca.dropna()
+
 # Brush for selection
 brush = alt.selection_interval()
 
@@ -106,82 +236,99 @@ if df[health_outcome_of_interest].dtype == 'object':
     points = alt.Chart(df_pca).mark_circle().encode(
         x='PC1:Q',
         y='PC2:Q',
-        color=alt.condition(brush, 'Health Outcome', alt.value('grey')),
+        color=alt.condition(brush, alt.Color('Health Outcome:N', legend=alt.Legend(title=f'{health_outcome_of_interest}')), alt.value('grey')),
         tooltip=['Health Outcome', 'PC1', 'PC2']
+    ).properties(
+        title=f'PCA Plot of Lifestyle Factors for \"{health_outcome_of_interest}\"',
+        width=600,
+        height=600
     ).add_params(
         brush
     )
     # for each category, calculate the percentage of each category in the selected
-    ranked_text = alt.Chart(df_pca).mark_text(align='right').encode(
+    ranked_text = alt.Chart(df_pca).mark_text(align='left', fontSize=16).encode(
             y=alt.Y('row_number:O').axis(None)
         ).transform_filter(
             brush
         )
-    points
-    # calculate percentage for each category, do not use df_pca because brush is applied
-    # categories = df_pca['Health Outcome'].unique()
-    # text_lst = []
-    # for category in categories:
-    #     temp_text = ranked_text.transform_filter(alt.datum['Health Outcome'] == category)
-    #     this_cate_count = temp_text.data.shape[0]
-    #     total = ranked_text.data.shape[0]
-    #     percentage = this_cate_count / total
-    #     text = temp_text.encode(text=alt.value(f'{category}: {percentage:.2%}')).properties(
-    #         title=alt.Title(text=f'{category}', align='right')
-    #     )
-    #     text_lst.append(text)
-    # text = alt.hconcat(*text_lst)        
-    # points & text
-    # ranked_text = alt.Chart(df_pca).mark_text(align='right').encode(
-    #         y=alt.Y('row_number:O').axis(None)
-    #     ).transform_filter(
-    #         brush
-    #     )
-    # # calculate percentage for each category
-    # total = df_pca.shape[0]
-    # unique_values = df_pca['Health Outcome'].unique()
-    # text_lst = []
-    # for category in unique_values:
-    #     percentage = df_pca[df_pca['Health Outcome'] == category].shape[0] / total
-    #     text = ranked_text.encode(text=alt.value(f'{category}: {percentage:.2%}')).properties(
-    #         title=alt.Title(text=f'{category}', align='right')
-    #     )
-    #     text_lst.append(text)
-    # text = alt.hconcat(*text_lst)
-    # points & text
+    categories = df_pca['Health Outcome'].unique()
+    text_lst = []
+    for category in categories:
+        
+        ranked_text.encode(text='mean(Health Outcome)').properties(
+            title=alt.Title(text='Mean Health Outcome', align='left')
+        )
+        
+        temp_text = ranked_text.transform_filter(
+            alt.datum['Health Outcome'] == category
+            ).encode(
+                text='count(Health Outcome)'
+            ).properties(
+                title=alt.Title(text=f'{category} Count', align='left')
+            )
+        text_lst.append(temp_text)
+        
+    # histogram for the selected data
+    health_outcome_hist = alt.Chart(df_pca).mark_bar().encode(
+        x=alt.X('Health Outcome:N', title=f'{health_outcome_of_interest}'),
+        y='count()',
+        color=alt.value('steelblue')
+    ).transform_filter(
+        brush
+    ).properties(
+        title=f'Distribution of {health_outcome_of_interest} in Selected Data',
+        width=600,
+        height=200
+    )
+    text = alt.vconcat(*text_lst)
+    points & text & health_outcome_hist
 else:
     # Scatter Plot
     points = alt.Chart(df_pca).mark_circle().encode(
         x='PC1:Q',
         y='PC2:Q',
-        color=alt.condition(brush, 'Health Outcome:Q', alt.value('grey')),
+        color=alt.condition(brush, alt.Color('Health Outcome:Q', legend=alt.Legend(title=f'{health_outcome_of_interest}')), alt.value('grey')),
         tooltip=['Health Outcome', 'PC1', 'PC2']
+    ).properties(
+        title=f'PCA Plot of Lifestyle Factors for \"{health_outcome_of_interest}\"',
+        width=600,
+        height=600
     ).add_params(
         brush
     )
-    ranked_text = alt.Chart(df_pca).mark_text(align='right').encode(
+    ranked_text = alt.Chart(df_pca).mark_text(align='left', fontSize=16).encode(
             y=alt.Y('row_number:O').axis(None)
         ).transform_filter(
             brush
         )
     mean_Health_Outcome = ranked_text.encode(text='mean(Health Outcome)').properties(
-            title=alt.Title(text='Mean Health Outcome', align='right')
+            title=alt.Title(text=f'Mean {health_outcome_of_interest}', align='left')
         )
     median_Health_Outcome = ranked_text.encode(text='median(Health Outcome)').properties(
-            title=alt.Title(text='Median Health Outcome', align='right')
+            title=alt.Title(text=f'Median {health_outcome_of_interest}', align='left')
         )
-    std_Health_Outcome = ranked_text.encode(text='stdev(Health Outcome)').properties(
-            title=alt.Title(text='Standard Deviation Health Outcome', align='right')
+    std_Health_Outcome = ranked_text.encode(text='stdev(Health Outcome)').properties(            
+            title=alt.Title(text=f'Standard Deviation {health_outcome_of_interest}', align='left')
         )
-    text = alt.hconcat(
+    text = alt.vconcat(
             mean_Health_Outcome,
             median_Health_Outcome,
             std_Health_Outcome,
         )
-    points & text
+    # add a histogram for the selected data
+    health_outcome_hist = alt.Chart(df_pca).mark_bar().encode(
+        x=alt.X('Health Outcome:Q', bin=alt.Bin(maxbins=30), title=f'{health_outcome_of_interest}'),
+        y='count()',
+        color=alt.value('steelblue')
+    ).transform_filter(
+        brush
+    ).properties(
+        title=f'Distribution of {health_outcome_of_interest} in Selected Data',
+        width=600,
+        height=200
+    )
+    # align point and text to stack vertically
+    points & text & health_outcome_hist
     
-
-
-# points
 
 
